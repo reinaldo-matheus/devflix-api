07a0fdb22f97bd9bd50f73a501cde522835e6dc2
reinaldo-matheus <reinaldomatheusdeveloper@gmail.com>
2025-06-24 14:03:09 -0300

    feat: adionada a ordenação, gênero e idioma do filme

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { PrismaClient } from '@prisma/client';
 
+
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: 'asc',
+        }, 
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
     res.json(movies);
 });
 
