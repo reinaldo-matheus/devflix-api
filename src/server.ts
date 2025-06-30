5738a7f4a92ee2ca205d0e375c32d64f807783ea
reinaldo-matheus <reinaldomatheusdeveloper@gmail.com>
2025-06-30 15:27:41 -0300

    feat: adionado remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -82,7 +82,24 @@ res.status(500).send({message: "Falha ao atualizar o registro do filme"})
 
 //retornar o status correto informando que o filme foi atualizado com sucesso:
 res.status(200).send();
+
+app.delete('/mives/:id', async (req, res)=>{
+  const id = Number(req.params.id)
+    try{
+const movie = await prisma.movie.findUnique({ where: {id}});
+
+if (!movie){
+  return res.status(404).send({ message: "O filme não foi encontrado!"})
+}
+
+  await prisma.movie.delete({ where: {id}});
+    }catch(error){
+return res.status(500).send({ message: "Não foi possível remover o filme!"})
+
+    }
+  res.status(200).send()
 });
+
 app.listen(port, () => {
   console.log(`servidor de exeção na porta ${port}`);
 });
